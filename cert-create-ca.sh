8dcef7f554046e38ee32e61cacc7a10152a5f0f5
Chris Ruettimann <chris@bitbull.ch>
2017-10-31 14:55:38 +0100

    bugfix, dirs not created

--- cert-create-ca.sh
+++ cert-create-ca.sh
@@ -29,8 +29,10 @@ fi
 
 # cert fqdn
 HOST="$1"
-# cert root dir
-BASE_DIR="/etc/MySsl/$HOST"
+# ca dir
+CA_DIR="/etc/MySsl"
+# cert dir
+BASE_DIR="$CA_DIR/$HOST"
 # certificate details
 CA_SUBJ='/C=CH/O=Bitbull-CA/CN=LabAuthority'
 CERT_SUBJ='/C=CH/ST=SG/L=Flawil/O=Bitbull'
@@ -46,41 +48,42 @@ CERT_COPY="/etc/nginx/${HOST}_cert.pem"
 CA_CHAIN_COPY="/etc/nginx/${HOST}_ca_chain.pem"
 COPY_OWNER="root:nginx"
 
-mkdir -p ${BASE_DIR}/{ca,servers,tmp}
+mkdir -p ${CA_DIR}/ca
+mkdir -p ${BASE_DIR}/{servers,tmp}
 cd "${BASE_DIR}" || exit 1
-if [ -f "ca/root.key.pem" ]
+if [ -f "${CA_DIR}/ca/root.key.pem" ]
 then
-   echo "------ Skip creation of ca/root.key.pem, it is already present"
+   echo "------ Skip creation of ${CA_DIR}/ca/root.key.pem, it is already present"
 else
    echo "------ create root key"
-   openssl genrsa -out "ca/root.key.pem" $CA_KEYLEN
+   openssl genrsa -out "${CA_DIR}/ca/root.key.pem" $CA_KEYLEN
 fi
-if [ -f "ca/root.crt.pem" ]
+if [ -f "${CA_DIR}/ca/root.crt.pem" ]
 then
-   echo "------ Skip creation of ca/root.crt.pem, it is already present"
+   echo "------ Skip creation of ${CA_DIR}/ca/root.crt.pem, it is already present"
 else
    echo "------ create intermediate cert"
-   openssl req -x509 -new -nodes -key "ca/root.key.pem" -days $DAYS -out "ca/root.crt.pem" -subj "$CA_SUBJ"
+   openssl req -x509 -new -nodes -key "${CA_DIR}/ca/root.key.pem" -days $DAYS -out "${CA_DIR}/ca/root.crt.pem" -subj "$CA_SUBJ"
 fi
-if [ -f "servers/${HOST}_privkey.pem" ]
+if [ -f "${BASE_DIR}/servers/${HOST}_privkey.pem" ]
 then
-   echo "------ ERROR: servers/${HOST}_privkey.pem this file should not exist, remove this files first:"
-   ls -l servers/${HOST}_*
+   echo "------ ERROR: ${BASE_DIR}/servers/${HOST}_privkey.pem this file should not exist, remove this files first:"
+   ls -l ${BASE_DIR}/servers/${HOST}_*
 else
    echo "------ create server private key"
-   openssl genrsa -out "servers/${HOST}_privkey.pem" $PRIV_KEYLEN -key "servers/${HOST}_privkey.pem"
+   openssl genrsa -out "${BASE_DIR}/servers/${HOST}_privkey.pem" $PRIV_KEYLEN -key "${BASE_DIR}/servers/${HOST}_privkey.pem"
    echo "------ create server csr"
-   openssl req -key "servers/${HOST}_privkey.pem" -new -sha256 -out "tmp/${HOST}.csr.pem" -subj "$CERT_SUBJ/CN=${HOST}"
+   openssl req -key "${BASE_DIR}/servers/${HOST}_privkey.pem" -new -sha256 -out "tmp/${HOST}.csr.pem" -subj "$CERT_SUBJ/CN=${HOST}"
    echo "sign csr"
-   openssl x509 -req -in "tmp/${HOST}.csr.pem" -CA "ca/root.crt.pem" -CAkey "ca/root.key.pem" -CAcreateserial -out "servers/${HOST}_cert.pem" -days $DAYS
+   openssl x509 -req -in "tmp/${HOST}.csr.pem" -CA "${CA_DIR}/ca/root.crt.pem" -CAkey "${CA_DIR}/ca/root.key.pem" -CAcreateserial -out "${BASE_DIR}/servers/${HOST}_cert.pem" -days $DAYS
    echo "------ prepare cert chain"
-   cat "ca/root.crt.pem" > "servers/${HOST}_ca_chain.pem"
+   cat "${CA_DIR}/ca/root.crt.pem" > "${BASE_DIR}/servers/${HOST}_ca_chain.pem"
    echo " ------ SHOW RESULTS"
    find "$BASE_DIR"
    echo "------ copy cert in place"
-   cp -v "servers/${HOST}_privkey.pem" "$KEY_COPY" ; chmod 640 "$KEY_COPY"
-   cp -v "servers/${HOST}_cert.pem" "$CERT_COPY" ; chmod 644 "$CERT_COPY" 
-   cp -v "servers/${HOST}_ca_chain.pem" "$CA_CHAIN_COPY" ; chmod 644 "$CA_CHAIN_COPY"
+   cp -v "${BASE_DIR}/servers/${HOST}_privkey.pem" "$KEY_COPY" ; chmod 640 "$KEY_COPY"
+   cp -v "${BASE_DIR}/servers/${HOST}_cert.pem" "$CERT_COPY" ; chmod 644 "$CERT_COPY" 
+   cp -v "${BASE_DIR}/servers/${HOST}_ca_chain.pem" "$CA_CHAIN_COPY" ; chmod 644 "$CA_CHAIN_COPY"
    chown $COPY_OWNER "$KEY_COPY" "$CERT_COPY" "$CA_CHAIN_COPY"
    echo "--- done"
 fi
