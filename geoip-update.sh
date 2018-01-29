556daa961bf1aa8308c03f083ebc616a7aad6cc7
Chris Ruettimann <chris@bitbull.ch>
2018-01-29 15:12:30 +0100

    added city db v1

--- geoip-update.sh
+++ geoip-update.sh
@@ -19,11 +19,13 @@ rm -f GeoLite2-Country.*
 wget http://geolite.maxmind.com/download/geoip/database/GeoLite2-City.mmdb.gz
 wget http://geolite.maxmind.com/download/geoip/database/GeoLite2-Country.mmdb.gz
 curl http://geolite.maxmind.com/download/geoip/database/GeoLiteCountry/GeoIP.dat.gz | gzip -d - > /srv/geoip/GeoIP.dat
+curl http://geolite.maxmind.com/download/geoip/database/GeoLiteCity.dat.gz | gzip -d - > GeoLiteCity.dat.gz
 gunzip GeoLite2-City.mmdb.gz
 gunzip GeoLite2-Country.mmdb.gz
 
 strings /srv/geoip/GeoLite2-City.mmdb | tail -20 | grep -q GeoLite || exit 1
 strings /srv/geoip/GeoLite2-Country.mmdb | tail -20 | grep -q GeoLite || exit 1
 strings /srv/geoip/GeoIP.dat | tail -20 | grep -q MaxMind || exit 1
+strings /srv/geoip/GeoLiteCity.dat.gz | tail -20 | grep -q MaxMind || exit 1
 exit 0
 
