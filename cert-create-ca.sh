47a7bc492362d2ea6282e6d507553f6bdeb366b9
Chris Ruettimann <chris@bitbull.ch>
2018-03-25 19:34:24 +0200

    updated to make wildcard certs possible: *.domain.com -> star.domain.com filename

--- cert-create-ca.sh
+++ cert-create-ca.sh
@@ -20,21 +20,16 @@ then
    exit 1
 fi
 
-ping -c1 -w1 -- "$1" 2>&1 | grep -q "Name or service not known"
-if [ $? -eq 0 ]
-then
-   echo "------ WARN: can not resolve host $1"
-   exit 1
-fi
-
 # cert fqdn
 HOST="$1"
+# file for fqdn which is wildcard aware
+HOSTF="$(echo $1 | sed 's/*/star/g')"
 # ca dir
-CA_DIR="/etc/MySsl"
+CA_DIR="$HOME/MySsl"
 # cert dir
-BASE_DIR="$CA_DIR/$HOST"
+BASE_DIR="$CA_DIR/$HOSTF"
 # certificate details
-CA_SUBJ='/C=CH/O=Bitbull-CA/CN=LabAuthority'
+CA_SUBJ='/C=CH/O=Bitbull-CA/CN=LAB-Auth'
 CERT_SUBJ='/C=CH/ST=SG/L=Flawil/O=Bitbull'
 # cert validity days
 DAYS=3650
@@ -42,11 +37,6 @@ DAYS=3650
 CA_KEYLEN=4096
 # cert keylen
 PRIV_KEYLEN=2048
-
-KEY_COPY="/etc/nginx/${HOST}_privkey.pem"
-CERT_COPY="/etc/nginx/${HOST}_cert.pem"
-CA_CHAIN_COPY="/etc/nginx/${HOST}_ca_chain.pem"
-COPY_OWNER="root:nginx"
 
 mkdir -p ${CA_DIR}/ca
 mkdir -p ${BASE_DIR}/{servers,tmp}
@@ -65,25 +55,20 @@ else
    echo "------ create intermediate cert"
    openssl req -x509 -new -nodes -key "${CA_DIR}/ca/root.key.pem" -days $DAYS -out "${CA_DIR}/ca/root.crt.pem" -subj "$CA_SUBJ"
 fi
-if [ -f "${BASE_DIR}/servers/${HOST}_privkey.pem" ]
+if [ -f "${BASE_DIR}/servers/${HOSTF}_privkey.pem" ]
 then
-   echo "------ ERROR: ${BASE_DIR}/servers/${HOST}_privkey.pem this file should not exist, remove this files first:"
-   ls -l ${BASE_DIR}/servers/${HOST}_*
+   echo "------ ERROR: ${BASE_DIR}/servers/${HOSTF}_privkey.pem this file should not exist, remove this files first:"
+   ls -l ${BASE_DIR}/servers/${HOSTF}_*
 else
    echo "------ create server private key"
-   openssl genrsa -out "${BASE_DIR}/servers/${HOST}_privkey.pem" $PRIV_KEYLEN -key "${BASE_DIR}/servers/${HOST}_privkey.pem"
+   openssl genrsa -out "${BASE_DIR}/servers/${HOSTF}_privkey.pem" $PRIV_KEYLEN -key "${BASE_DIR}/servers/${HOSTF}_privkey.pem"
    echo "------ create server csr"
-   openssl req -key "${BASE_DIR}/servers/${HOST}_privkey.pem" -new -sha256 -out "tmp/${HOST}.csr.pem" -subj "$CERT_SUBJ/CN=${HOST}"
+   openssl req -key "${BASE_DIR}/servers/${HOSTF}_privkey.pem" -new -sha256 -out "tmp/${HOSTF}.csr.pem" -subj "$CERT_SUBJ/CN=${HOST}"
    echo "sign csr"
-   openssl x509 -req -in "tmp/${HOST}.csr.pem" -CA "${CA_DIR}/ca/root.crt.pem" -CAkey "${CA_DIR}/ca/root.key.pem" -CAcreateserial -out "${BASE_DIR}/servers/${HOST}_cert.pem" -days $DAYS
+   openssl x509 -req -in "tmp/${HOSTF}.csr.pem" -CA "${CA_DIR}/ca/root.crt.pem" -CAkey "${CA_DIR}/ca/root.key.pem" -CAcreateserial -out "${BASE_DIR}/servers/${HOSTF}_cert.pem" -days $DAYS
    echo "------ prepare cert chain"
-   cat "${CA_DIR}/ca/root.crt.pem" > "${BASE_DIR}/servers/${HOST}_ca_chain.pem"
+   cat "${CA_DIR}/ca/root.crt.pem" > "${BASE_DIR}/servers/${HOSTF}_ca_chain.pem"
    echo " ------ SHOW RESULTS"
    find "$BASE_DIR"
-   echo "------ copy cert in place"
-   cp -v "${BASE_DIR}/servers/${HOST}_privkey.pem" "$KEY_COPY" ; chmod 640 "$KEY_COPY"
-   cp -v "${BASE_DIR}/servers/${HOST}_cert.pem" "$CERT_COPY" ; chmod 644 "$CERT_COPY" 
-   cp -v "${BASE_DIR}/servers/${HOST}_ca_chain.pem" "$CA_CHAIN_COPY" ; chmod 644 "$CA_CHAIN_COPY"
-   chown $COPY_OWNER "$KEY_COPY" "$CERT_COPY" "$CA_CHAIN_COPY"
    echo "--- done"
 fi
