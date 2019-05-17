399df58c55cfba5b00dc61447f2a1f38db6b5993
Chris Ruettimann <chris@bitbull.ch>
2019-05-17 10:43:33 +0200

    first release

---
+++ vcsa_67_backup_scp.sh
@@ -0,0 +1,105 @@
+#!/bin/bash
+# DESC: VCSA 6.7 scp backup
+# Copyright (c) Chris Ruettimann <chris@bitbull.ch>
+
+# This software is licensed to you under the GNU General Public License.
+# There is NO WARRANTY for this software, express or
+# implied, including the implied warranties of MERCHANTABILITY or FITNESS
+# FOR A PARTICULAR PURPOSE. You should have received a copy of GPLv2
+# along with this software; if not, see
+# http://www.gnu.org/licenses/gpl.txt
+# INSPIRED BY: https://pubs.vmware.com/vsphere-6-5/index.jsp?topic=%2Fcom.vmware.vsphere.vcsapg-rest.doc%2FGUID-222400F3-678E-4028-874F-1F83036D2E85.html 
+
+# YOU CAN ADD THIS TO CRON ON DESTINATION SERVER ######################################
+# chmod 0700 /home/vbackup/bin/vcsa_67_backup_scp.sh
+# crontab -e $BACKUP_USER
+# 1 2 * * * /home/vbackup/bin/vcsa_67_backup_scp.sh
+
+##### EDITABLE BY USER to specify vCenter Server instance and backup destination. #####
+VC_ADDRESS='vcenter1.example.com'
+VC_USER='administrator@vsphere.local'
+VC_PASSWORD='letmein!'
+BACKUP_ADDRESS='192.168.10.55'
+BACKUP_USER='vbackup'
+BACKUP_PASSWORD='backmeup!'
+BACKUP_FOLDER="/home/vbackup/$VC_ADDRESS"
+TIME=$(date +%Y-%m-%d-%H-%M-%S)
+BACKUP_INVENTORY="$BACKUP_FOLDER/$TIME/backup-metadata.json"
+BACKUP_KEEP=2 # keep this amount of backups in place
+############################
+
+# Authenticate with basic credentials.
+curl -s -u "$VC_USER:$VC_PASSWORD" \
+   -X POST \
+   -k --cookie-jar cookies.txt \
+   "https://$VC_ADDRESS/rest/com/vmware/cis/session"
+
+# Create a message body for the backup request.
+cat << EOF >task.json
+{ "piece":
+     {
+         "location_type":"SCP",
+         "comment":"Automatic backup",
+         "parts":["seat"],
+         "location":"scp://$BACKUP_ADDRESS/$BACKUP_FOLDER/$TIME",
+         "location_user":"$BACKUP_USER",
+         "location_password":"$BACKUP_PASSWORD"
+     }
+}
+EOF
+
+# Issue a request to start the backup operation.
+logger -t $(basename $0) "Starting backup $TIME"
+curl -s -k --cookie cookies.txt \
+   -H 'Accept:application/json' \
+   -H 'Content-Type:application/json' \
+   -X POST \
+   --data @task.json 2>>response.err >response.txt \
+   "https://$VC_ADDRESS/rest/appliance/recovery/backup/job"
+cat response.err | logger -t $(basename $0)
+cat response.txt | logger -t $(basename $0)
+
+# Parse the response to locate the unique identifier of the backup operation.
+ID=$(awk '{if (match($0,/"id":"\w+-\w+-\w+"/)) \
+          print substr($0, RSTART+6, RLENGTH-7);}' \
+         response.txt)
+logger -t $(basename $0) "Backup job id: $ID"
+
+# Monitor progress of the operation until it is complete.
+PROGRESS=INPROGRESS
+until [ "$PROGRESS" != "INPROGRESS" ]
+do
+     sleep 10s
+     curl -s -k --cookie cookies.txt \
+       -H 'Accept:application/json' \
+       --globoff \
+       "https://$VC_ADDRESS/rest/appliance/recovery/backup/job/$ID" \
+       >response.txt
+     cat response.txt | logger -t $(basename $0)
+     PROGRESS=$(awk '{if (match($0,/"state":"\w+"/)) \
+                     print substr($0, RSTART+9, RLENGTH-10);}' \
+                    response.txt)
+     echo "Backup job state: $PROGRESS"
+     logger -t $(basename $0) "Backup job state: $PROGRESS"
+done
+
+# Report job completion and clean up temporary files.
+logger -t $(basename $0) "Backup job completion status: $PROGRESS"
+rm -f task.json
+rm -f response.txt
+rm -f response.err
+rm -f cookies.txt
+
+if [ "$PROGRESS" == "SUCCEEDED" ]
+then
+  egrep 'BackupSize|StartTime|Duration|EndTime' $BACKUP_INVENTORY | sed 's/["|,]//g' | logger -t $(basename $0)
+  logger -t $(basename $0) "INFO: removing old backups if needed. We keep $BACKUP_KEEP of them"
+  test -d $BACKUP_FOLDER || exit 1
+  ls -1d $BACKUP_FOLDER/*-*-*-*-* | sort -n | head -n-$BACKUP_KEEP | xargs rm -frv | logger -t $(basename $0)
+  logger -t $(basename $0) "INFO: backup finished"
+  echo "INFO: backup finished"
+else
+  echo "ERROR: backup failed, please check syslog"
+  logger -t $(basename $0) "ERROR: backup failed"
+fi
+
---
+++ vcsa_67_restore_scp.sh
@@ -0,0 +1,79 @@
+#!/bin/bash
+# DESC: VCSA 6.7 scp backup
+# Copyright (c) Chris Ruettimann <chris@bitbull.ch>
+
+# This software is licensed to you under the GNU General Public License.
+# There is NO WARRANTY for this software, express or
+# implied, including the implied warranties of MERCHANTABILITY or FITNESS
+# FOR A PARTICULAR PURPOSE. You should have received a copy of GPLv2
+# along with this software; if not, see
+# http://www.gnu.org/licenses/gpl.txt
+
+##### EDITABLE BY USER to specify vCenter Server instance and backup destination. #####
+VC_ADDRESS='vcenter1.lab.local'
+VC_USER='root'
+VC_PASSWORD='root.secret'
+BACKUP_ADDRESS='192.168.10.55'
+BACKUP_USER='vbackup'
+BACKUP_PASSWORD='backup.secret'
+BACKUP_FOLDER="/home/vbackup/vcenter1.lab.local/2018-03-08-13-40-22"
+TIME=$(date +%Y-%m-%d-%H-%M-%S)
+############################
+
+# Create a message body for the restore request.
+cat << EOF > task.json
+{ "piece":
+    {
+        "location_type":"SCP",
+        "location":"scp://$BACKUP_ADDRESS/$BACKUP_FOLDER",
+        "location_user":"$BACKUP_USER",
+        "location_password":"$BACKUP_PASSWORD"
+    }
+}
+EOF
+
+# Issue a request to start the restore operation.
+echo Starting restore $TIME $VC_B64_PASS >> restore.log
+curl -s -k -u "$VC_USER:$VC_PASSWORD" \
+  -H 'Accept:application/json' \
+  -H 'Content-Type:application/json' \
+  -X POST \
+  "https://$VC_ADDRESS:5480/rest/com/vmware/appliance/recovery/restore/job" \
+  --data @task.json 2>restore.log >response.txt
+cat response.txt >> restore.log
+echo '' >> restore.log
+
+# Monitor progress of the operation until it is complete.
+STATE=STARTING
+PROGRESS=0
+until [ "$STATE" != "INPROGRESS" -a "$STATE" != "STARTING" ]
+do
+    echo "Restore job state: $STATE ($PROGRESS%)"
+    sleep 10s
+    curl -s -k -u "$VC_USER:$VC_PASSWORD" \
+      -H 'Accept:application/json' \
+      -H 'Content-Type:application/json' \
+      -X POST -d '' \
+      "https://$VC_ADDRESS:5480/rest/com/vmware/appliance/recovery/restore/job?~action=get" \
+      >response.txt
+    cat response.txt >> restore.log
+    echo '' >> restore.log
+    PROGRESS=$(awk \
+               '{if (match($0,/"progress":\w+/)) print substr($0, RSTART+11,RLENGTH-11);}' \
+               response.txt)
+    STATE=$(awk \
+            '{if (match($0,/"state":"\w+"/)) print substr($0, RSTART+9, RLENGTH-10);}' \
+            response.txt)
+done
+# Report job completion and clean up temporary files.
+echo ''
+echo Restore job has finished
+rm -f task.json
+rm -f response.txt
+echo '' >> restore.log
+echo
+echo "For more details about restore, check the vCSA restore log:
+      $VC_USER@$VC_ADDRESS:/storage/log/vmware/applmgmt/backupRestoreAPI.log"
+echo "============= restore.log ============="
+cat restore.log
+
