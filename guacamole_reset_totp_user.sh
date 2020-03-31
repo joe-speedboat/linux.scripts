b0edeae22f97eb9122eb1b454e5386a92899f451
Chris Ruettimann <chris@bitbull.ch>
2020-03-31 22:10:15 +0200

    debuggin, rewrite

--- guacamole_reset_totp_user.sh
+++ guacamole_reset_totp_user.sh
@@ -24,25 +24,30 @@ then
    echo "   USAGE: $(basename $0) <username>      # reset TOTP for this guacamole user"
    exit 1
 else
-   echo "   INFO, user $USR found in $DB database ..."
+   echo "   CHECK, user $USR found in $DB database ..."
 fi
 
+# fetch IDs for user
+USR_ENTITY=$(mysql $DB -N -B -e "SELECT entity_id FROM guacamole_entity WHERE name = '$USR' AND type = 'USER' ORDER BY entity_id LIMIT 1;")
+USR_ID=$(mysql $DB -N -B -e "SELECT affected_user_id FROM guacamole_user_permission WHERE entity_id = '$USR_ENTITY' ORDER BY affected_user_id LIMIT 1;")
 
-# check if user has enrolled totp before resetting the value
-mysql $DB -N -B -e "SELECT attribute_value FROM guacamole_user_attribute WHERE user_id IN (SELECT entity_id FROM guacamole_entity WHERE ( name = '$USR' AND type = 'USER') AND attribute_name = 'guac-totp-key-confirmed' );" | grep -q true
-if [ $? -ne 0 ]
+echo "   CHECK, reading current TOTP enrollment flag for user $USR ..."
+USR_ENROLLED=$(mysql $DB -N -B -e "SELECT attribute_value FROM guacamole_user_attribute WHERE user_id = '$USR_ID' AND attribute_name = 'guac-totp-key-confirmed';")
+echo "      DEBUG, USR=$USR / USR_ENTITY=$USR_ENTITY / USR_ID=$USR_ID / USR_ENROLLED=$USR_ENROLLED"
+
+if [ "$USR_ENROLLED" != 'true' ]
 then
-   echo "   ERROR, USER:$USR has no activated TOTP settings, so I do nothing"
+   echo "   ERROR, user $USR is curently not enrolled, so I do nothing"
    exit 1
 fi
 
-echo "   INFO, resetting TOTP enrollment flag for user $USR ..."
-mysql $DB -N -B -e "UPDATE guacamole_user_attribute SET attribute_value = 'false' WHERE ( user_id IN (SELECT entity_id FROM guacamole_entity WHERE name = '$USR' AND type = 'USER') AND attribute_name = 'guac-totp-key-confirmed' );"
+echo "   TASK, resetting TOTP enrollment flag for user $USR ..."
+mysql $DB -N -B -e "UPDATE guacamole_user_attribute SET attribute_value = 'false' WHERE ( user_id = '$USR_ID' AND attribute_name = 'guac-totp-key-confirmed' );"
 
-echo "   INFO, resetting TOTP secret for user $USR ..."
-mysql $DB -N -B -e "DELETE FROM guacamole_user_attribute WHERE user_id IN (SELECT entity_id FROM guacamole_entity WHERE ( name = '$USR' AND type = 'USER') AND attribute_name = 'guac-totp-key-secret' );"
+echo "   TASK, resetting TOTP secret for user $USR ..."
+mysql $DB -N -B -e "DELETE FROM guacamole_user_attribute WHERE user_id = '$USR_ID' AND attribute_name = 'guac-totp-key-secret';"
 
-mysql $DB -N -B -e "SELECT attribute_value FROM guacamole_user_attribute WHERE ( user_id IN (SELECT entity_id FROM guacamole_entity WHERE name = '$USR' AND type = 'USER') AND attribute_name = 'guac-totp-key-confirmed' );" | grep -q false
+mysql $DB -N -B -e "SELECT attribute_value FROM guacamole_user_attribute WHERE user_id = '$USR_ID' AND attribute_name = 'guac-totp-key-confirmed';" | grep -q false
 if [ $? -ne 0 ]
 then
    echo "   ERROR, could not reset TOTP settings for user $USR, please contact support@uniqconsulting.ch"
@@ -51,4 +56,5 @@ else
    echo "   INFO, TOTP settings for user $USR are now reseted and ready for re-enrollment, bye"
    exit 0
 fi
+
 
