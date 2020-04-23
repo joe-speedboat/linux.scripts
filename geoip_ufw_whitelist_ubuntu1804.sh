6b8b356e98c460bcd72e6c6f0833af9a457899fa
Chris Ruettimann <chris@bitbull.ch>
2020-04-23 08:01:25 +0200

    debugging

--- geoip_ufw_whitelist_ubuntu1804.sh
+++ geoip_ufw_whitelist_ubuntu1804.sh
@@ -105,13 +105,13 @@ set -e
 case "\$1" in
 start)
     iptables -A ufw-before-input -m set ! --match-set $IP_LIST src -j DROP
-    iptables -A ufw-before-input -m set ! --match-set $IP_LIST src -j LOG --log-prefix "[UFW BLOCK $IP_LIST] "
+    iptables -A ufw-before-input -m set ! --match-set $IP_LIST src -j LOG --log-prefix "[UFW BLOCK ALL EXCEPT $IP_LIST] "
     iptables -D ufw-before-input -j ufw-user-input
     iptables -A ufw-before-input -j ufw-user-input
     ;;
 stop)
     iptables -D ufw-before-input -m set ! --match-set $IP_LIST src -j DROP
-    iptables -D ufw-before-input -m set ! --match-set $IP_LIST src -j LOG --log-prefix "[UFW BLOCK $IP_LIST] "
+    iptables -D ufw-before-input -m set ! --match-set $IP_LIST src -j LOG --log-prefix "[UFW BLOCK ALL EXCEPT $IP_LIST] "
     ;;
 status)
     # optional
