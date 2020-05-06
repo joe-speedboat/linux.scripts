54b6090455937715e83269e1033bc52dbaf3a4ea
Chris Ruettimann <chris@bitbull.ch>
2020-05-06 08:31:32 +0200

    first release

---
+++ file2wiki.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+#########################################################################################
+# DESC: read files and create wiki like syntax
+#########################################################################################
+# Copyright (c) Chris Ruettimann <chris.ruettimann@uniqconsulting.ch>
+#
+# This software is licensed to you under the GNU General Public License.
+# There is NO WARRANTY for this software, express or
+# implied, including the implied warranties of MERCHANTABILITY or FITNESS
+# FOR A PARTICULAR PURPOSE. You should have received a copy of GPLv2
+# along with this software; if not, see
+# http://www.gnu.org/licenses/gpl.txt
+
+file2wiki(){
+if [ $# -gt 1 ] ; then
+   for f in $*
+   do
+      file2wiki $f
+   done
+fi
+
+f=$1
+echo "<div class=\"toccolours mw-collapsible mw-collapsed\" style=\"width:60%\">
+File: <b>$1</b>
+<div class=\"mw-collapsible-content\">
+<pre>"
+cat $1
+echo "</pre>
+</div>
+</div>
+"
+}
