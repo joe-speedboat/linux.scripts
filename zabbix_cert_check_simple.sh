853715abe71e803902a831dc1c0fd87dbce3d512
Chris Ruettimann <chris@bitbull.ch>
2021-03-17 13:44:38 +0100

    bugfix, port added to curl opts

--- zabbix_cert_check_simple.sh
+++ zabbix_cert_check_simple.sh
@@ -30,7 +30,7 @@ then
     port=443
 fi
 
-line=$(curl -kvv --max-time 5 https://$host 2>&1 | egrep 'issuer:|expire date:|start date:|subject:' | sed 's/.*: //' | tr -d ',' | tr '\n' ',')
+line=$(curl -kvv --max-time 5 https://$host:$port 2>&1 | egrep 'issuer:|expire date:|start date:|subject:' | sed 's/.*: //' | tr -d ',' | tr '\n' ',')
 issuer=$(echo $line | cut -d, -f4 | tr -d ';')
 subject=$(echo $line | cut -d, -f1 | tr -d ';' | sed 's/.*CN=/CN=/')
 start=$(echo $line | cut -d, -f2)
