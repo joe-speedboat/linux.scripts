83607a44dbf4fde606aeb2ed9fba019832f4d7b0
Chris Ruettimann <chris@bitbull.ch>
2021-03-29 20:55:48 +0200

    first release

---
+++ grow_cloud_rootfs_xfs.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+DISK=vda
+if [ $(lsblk | grep ^vda | awk '{print $4}' ) != $(df -hP / | grep /dev/$DISK | awk '{print $2}') ]
+then
+  echo -e 'd\nw\n' | fdisk /dev/$DISK
+  echo -e 'n\np\n\n\n\n\nw\n' | fdisk /dev/$DISK
+  partprobe /dev/$DISK
+  xfs_growfs /
+fi
