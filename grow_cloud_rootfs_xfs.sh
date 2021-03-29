6aef0db3e91132739973e79b9bb87ecd40d55b52
Chris Ruettimann <chris@bitbull.ch>
2021-03-29 21:29:37 +0200

    path added

--- grow_cloud_rootfs_xfs.sh
+++ grow_cloud_rootfs_xfs.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+export PATH=/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin
 DISK=vda
 if [ $(lsblk | grep ^vda | awk '{print $4}' ) != $(df -hP / | grep /dev/$DISK | awk '{print $2}') ]
 then
