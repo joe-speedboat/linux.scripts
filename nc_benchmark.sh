8cc90425c6a3f28277d6e88a39373d7eb1fce2bd
Chris Ruettimann <chris@bitbull.ch>
2021-04-01 17:42:06 +0200

    first release

---
+++ nc_benchmark.sh
@@ -0,0 +1,154 @@
+#!/bin/bash
+#########################################################################################
+# DESC: nextcloud webdav benchmark testet with v21
+#########################################################################################
+# Copyright (c) Chris Ruettimann <chris@bitbull.ch>
+
+# This software is licensed to you under the GNU General Public License.
+# There is NO WARRANTY for this software, express or
+# implied, including the implied warranties of MERCHANTABILITY or FITNESS
+# FOR A PARTICULAR PURPOSE. You should have received a copy of GPLv2
+# along with this software; if not, see
+# http://www.gnu.org/licenses/gpl.txt
+
+# HOW IT WORKS:
+# 1) upload one big file
+# 2) download one big file
+# 3) upload many small files
+# 4) download many small files
+# 5) download previous result file from nextcloud if it exists
+# 6) add the results to the result file
+# 7) upload the results to nextcloud
+# 8) cleanup the files
+#
+# INSTALL:
+#   1) curl https://raw.githubusercontent.com/joe-speedboat/shell.scripts/master/nc_benchmark.sh > /usr/local/sbin/nc_benchmark.sh
+#   2) chmod 700 /usr/local/sbin/nc_benchmark.sh
+#   3) vim /usr/local/sbin/nc_benchmark.sh # Fill at least this vars
+#         CLOUD
+#         PW
+#         USR
+#   4) /usr/local/sbin/nc_benchmark.sh
+#   5) optionally create cronjob to update results on a sheduled base
+#   6) optionally create config file to source the settings $SCRIPT_NAME.conf
+
+# USEAGE:
+#         nc_benchmark.sh
+
+
+# custom vars ---------------
+CLOUD=cloud.domain.com
+USR="chris"
+PW="jRmkt-xxx-2YBQk"
+TEST_BLOCK_SIZE_MB=150
+TEST_FILES_COUNT=100
+BENCH_DIR="bench"
+# ---------------------------
+
+cd $(dirname $0)
+
+if [ -r "$(basename $0).conf" ]
+then
+   echo "INFO: reading external config file: $(basename $0).conf"
+   source "$(basename $0).conf"
+fi
+
+# static vars
+BURL="https://$CLOUD"
+DAV_BASE_DIR="remote.php/dav"
+DAV_FILE_DIR="files/$USR"
+DAV_TRASH_DIR="trashbin/$USR/trash"
+DAV_FILE_URL="$BURL/$DAV_BASE_DIR/$DAV_FILE_DIR"
+DAV_TRASH_URL="$BURL/$DAV_BASE_DIR/$DAV_TRASH_DIR"
+DAV_REMOTE_BENCH_DIR="$DAV_FILE_URL/$BENCH_DIR"
+LOCAL_DIR="$HOME/.nc/$CLOUD"
+LOCAL_LOG_FILE="$LOCAL_DIR/$(basename $0).txt"
+CURL="curl -k -s -u$USR:$PW"
+
+# prepare local benchmark dirs
+test -d "$LOCAL_DIR" && rm -rf "$LOCAL_DIR"
+mkdir -p "$LOCAL_DIR/small_files"
+dd if=/dev/urandom of="$LOCAL_DIR/$TEST_BLOCK_SIZE_MB.mb" bs=1M count=$TEST_BLOCK_SIZE_MB >/dev/null 2>&1
+for i in $(seq 1 $TEST_FILES_COUNT)
+do
+   date > $LOCAL_DIR/small_files/$i.txt
+done
+
+# prepare remote benchmark dirs
+$CURL $DAV_REMOTE_BENCH_DIR/$(basename $LOCAL_LOG_FILE) -o "$LOCAL_LOG_FILE" 2>/dev/null 
+cat "$LOCAL_LOG_FILE" 2>/dev/null | grep -q DATE || echo '#DATE;BURL;USER;<UPLOAD|DOWNLOAD>;TEST;ERRORS;RESULTS' >  $LOCAL_LOG_FILE
+$CURL "$DAV_REMOTE_BENCH_DIR/small_files/0.txt"  >/dev/null 2>&1 && $CURL -X DELETE "$DAV_REMOTE_BENCH_DIR/small_files/" >/dev/null 2>&1
+$CURL -X MKCOL "$DAV_REMOTE_BENCH_DIR" >/dev/null 2>&1
+$CURL -X MKCOL "$DAV_REMOTE_BENCH_DIR/small_files" >/dev/null 2>&1
+$CURL -X DELETE "$DAV_REMOTE_BENCH_DIR/$TEST_BLOCK_SIZE_MB.mb" >/dev/null 2>&1
+
+# run block upload test
+echo upload $TEST_BLOCK_SIZE_MB MB
+UL_BLOCK_SPEED=$($CURL -w '%{speed_upload}' -T "$LOCAL_DIR/$TEST_BLOCK_SIZE_MB.mb" "$DAV_REMOTE_BENCH_DIR/" | cut -d. -f1)
+UL_BLOCK_SPEED=$(( $UL_BLOCK_SPEED / 1024 )) # kbyte per sec
+rm -f "$LOCAL_DIR/$TEST_BLOCK_SIZE_MB.mb"
+# run block download test
+echo download $TEST_BLOCK_SIZE_MB MB
+DL_BLOCK_SPEED=$($CURL -w '%{speed_download}' "$DAV_REMOTE_BENCH_DIR/$TEST_BLOCK_SIZE_MB.mb" -o "$LOCAL_DIR/$TEST_BLOCK_SIZE_MB.mb" | cut -d. -f1)
+DL_BLOCK_SPEED=$(( $DL_BLOCK_SPEED / 1024 )) # kbyte per sec
+rm -f "$LOCAL_DIR/$TEST_BLOCK_SIZE_MB.mb"
+
+# run small file upload test
+UL_ERROR_CNT=0
+TIME_BEFORE=$(date '+%s')
+for i in $(seq 1 $TEST_FILES_COUNT)
+do
+   echo upload file $i.txt | egrep '[0-9]0.txt'
+   $CURL  -T "$LOCAL_DIR/small_files/$i.txt" "$DAV_REMOTE_BENCH_DIR/small_files/" 
+   if [ $? -ne 0 ] ; then
+      echo "error: could not upload $i.txt"
+      UL_ERROR_CNT=$(($UL_ERROR_CNT+1))
+   fi
+done
+UL_FILES_TIME=$(( $(date '+%s') - $TIME_BEFORE))
+
+# run small file download test
+DL_ERROR_CNT=0
+TIME_BEFORE=$(date '+%s')
+for i in $(seq 1 $TEST_FILES_COUNT)
+do
+   echo download file $i.txt | egrep '[0-9]0.txt'
+   $CURL  -o "$LOCAL_DIR/small_files/$i.txt" "$DAV_REMOTE_BENCH_DIR/small_files/$i.txt" 
+   if [ $? -ne 0 ] ; then
+      echo "error: could not download $i.txt"
+      DL_ERROR_CNT=$(($DL_ERROR_CNT+1))
+   fi
+done
+DL_FILES_TIME=$(( $(date '+%s') - $TIME_BEFORE))
+
+echo BURL=$BURL
+echo TEST_BLOCK_SIZE_MB=$TEST_BLOCK_SIZE_MB
+echo UL_BLOCK_SPEED=$UL_BLOCK_SPEED KByte/s
+echo DL_BLOCK_SPEED=$DL_BLOCK_SPEED KByte/s
+echo TEST_FILES_COUNT=$TEST_FILES_COUNT
+echo DL_ERROR_CNT=$DL_ERROR_CNT
+echo UL_ERROR_CNT=$UL_ERROR_CNT
+echo UL_FILES_TIME=$UL_FILES_TIME sec
+echo DL_FILES_TIME=$DL_FILES_TIME sec
+
+D="$(date '+%Y.%m.%d %H:%M:%S')"
+echo "$D;$BURL;$USR;UPLOAD;Block $TEST_BLOCK_SIZE_MB MB;;$UL_BLOCK_SPEED KByte/s" >>  $LOCAL_LOG_FILE
+echo "$D;$BURL;$USR;DOWNLOAD;Block $TEST_BLOCK_SIZE_MB MB;;$DL_BLOCK_SPEED KByte/s" >>  $LOCAL_LOG_FILE
+echo "$D;$BURL;$USR;UPLOAD;$TEST_FILES_COUNT small Files;$UL_ERROR_CNT;$UL_FILES_TIME sec" >>  $LOCAL_LOG_FILE
+echo "$D;$BURL;$USR;DOWNLOAD;$TEST_FILES_COUNT small Files;$DL_ERROR_CNT;$DL_FILES_TIME sec" >>  $LOCAL_LOG_FILE
+echo uploading results: $LOCAL_LOG_FILE to $DAV_REMOTE_BENCH_DIR
+$CURL  -T "$LOCAL_LOG_FILE" "$DAV_REMOTE_BENCH_DIR/"
+echo "cleaning up test files"
+echo "   delete directory: $BENCH_DIR/small_files"
+$CURL -X DELETE "$DAV_REMOTE_BENCH_DIR/small_files/" >/dev/null 2>&1
+echo "   delete directory: $BENCH_DIR/$TEST_BLOCK_SIZE_MB.mb"
+$CURL -X DELETE "$DAV_REMOTE_BENCH_DIR/$TEST_BLOCK_SIZE_MB.mb" >/dev/null 2>&1
+sleep 5
+$CURL -X PROPFIND "$DAV_TRASH_URL" | sed "s|$DAV_TRASH_DIR/|\n|g" | sed 's|<.*||;/^$/!p' | sort -u | grep '[a-Z0-9]' | while read TRASH_FILE_TO_DELETE
+do
+  $CURL -X DELETE "$DAV_TRASH_URL/$TRASH_FILE_TO_DELETE"
+  echo "   delete trash file: ${TRASH_FILE_TO_DELETE}"
+done
+echo done
+
+
