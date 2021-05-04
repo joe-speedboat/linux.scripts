b99c49644cb2d4fe7dabd0b886f5d1bd6aa59990
Chris Ruettimann <chris@bitbull.ch>
2021-05-04 16:16:40 +0200

    first release

---
+++ openssl_check_cert_chain.sh
@@ -0,0 +1,51 @@
+#!/bin/bash
+#########################################################################################
+# DESC: check certificate chain with openssl
+#########################################################################################
+# Copyright (c) Chris Ruettimann <chris@bitbull.ch>
+
+# This software is licensed to you under the GNU General Public License.
+# There is NO WARRANTY for this software, express or
+# implied, including the implied warranties of MERCHANTABILITY or FITNESS
+# FOR A PARTICULAR PURPOSE. You should have received a copy of GPLv2
+# along with this software; if not, see
+# http://www.gnu.org/licenses/gpl.txt
+
+# found on:
+# https://kdecherf.com/blog/2015/04/10/show-the-certificate-chain-of-a-local-x509-file/
+
+#!/bin/bash
+
+chain_pem="${1}"
+
+if [[ ! -f "${chain_pem}" ]]; then
+    echo "Usage: $0 BASE64_CERTIFICATE_CHAIN_FILE" >&2
+    exit 1
+fi
+
+if ! openssl x509 -in "${chain_pem}" -noout 2>/dev/null ; then
+    echo "${chain_pem} is not a certificate" >&2
+    exit 1
+fi
+
+awk -F'\n' '
+        BEGIN {
+            showcert = "openssl x509 -noout -subject -issuer"
+        }
+
+        /-----BEGIN CERTIFICATE-----/ {
+            printf "%2d: ", ind
+        }
+
+        {
+            printf $0"\n" | showcert
+        }
+
+        /-----END CERTIFICATE-----/ {
+            close(showcert)
+            ind ++
+        }
+    ' "${chain_pem}"
+
+echo
+openssl verify -untrusted "${chain_pem}" "${chain_pem}"
