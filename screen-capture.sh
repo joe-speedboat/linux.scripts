05fb9928337574a3c012079e9ca025a8d122531b
Christian Ruettimann <chris@bitbull.ch>
2022-06-15 19:35:47 +0200

    moved to xfce tools

--- screen-capture.sh
+++ screen-capture.sh
@@ -19,7 +19,8 @@ test -d $DIR || mkdir $DIR
 test -f $TMP && TEXT="$(cat $TMP)"
 yad --title "Screen Shot Name" --entry --entry-text="$TEXT" --text "Enter Name of PNG Image:" > $TMP 2>/dev/null
 FILE="$(cat $TMP)_$(date '+%Y%m%d-%H%M%S')"
-scrot -s "$DIR/$FILE.png"
+# scrot -s "$DIR/$FILE.png"
+xfce4-screenshooter --region --save "$DIR/$FILE.png"
 exit 0
 
 ################################################################################
