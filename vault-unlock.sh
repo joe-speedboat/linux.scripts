33cc15f2b191d75fb7fc9a1e65f0c25f9899ac99
Christian Ruettimann <chris@bitbull.ch>
2022-08-27 11:17:25 +0200

    finishing

--- vault-unlock.sh
+++ vault-unlock.sh
@@ -11,13 +11,25 @@
 # http://www.gnu.org/licenses/gpl.txt
 
 
-# INSTALL:
+### INSTALL###
+# curl https://raw.githubusercontent.com/joe-speedboat/shell.scripts/master/vault-unlock.sh > /usr/local/bin/vault-unlock.sh
+# chmod 755 /usr/local/bin/vault-unlock.sh
 # sed -i 's#^.vault_password_file=.*#vault_password_file=/usr/local/bin/vault-unlock.sh#' /etc/ansible/ansible.cfg
+# add .bashrc snipped to ansible and ssh user
+# logout, login to ansible user
 
-echo '#!/bin/bash
-# without arg -> ask/print secret
+#!/bin/bash
+### DESCRIPTION ############################################################
+# Due lot of ansible work, I needed a script who can handle:
+# - ansible vault password handler , which is not persistent across reboots
+# - ansible vault password must not go into filesystem backup
+# - ssh-private-key passphrase is same as ansible vault password handlers secret
+# - must get called on first login via .bashrc, then it persists until machine reboot
+############################################################################
+# without arg -> ask/print secret key
 # arg: -r     -> read secret from stdin, only if not filled, do not print it
 # arg: -d     -> remove secret
+# arg: -D     -> remove secret key and ssh-agent
 
 ###### $HOME/.bashrc ######
 # /usr/local/bin/vault-unlock.sh -r
@@ -37,6 +49,14 @@ then
    keyctl purge user $NAME
    exit 0
 fi
+if [ "$1" == "-D" ]
+then
+   echo "INFO: removing key"
+   keyctl purge user $NAME
+   echo "INFO: removing ssh-agent"
+   ssh-add -D
+   exit 0
+fi
 
 PW_CNT=$(keyctl search @u user $NAME 2>/dev/null | wc -l)
 if [ $PW_CNT -lt 1 ]
@@ -48,13 +68,5 @@ else
    [ "$1" == "-r" ] && exit 0
    keyctl print $(keyctl search @u user $NAME 2>/dev/null)
 fi
-
-' > /usr/local/bin/vault-unlock.sh
-
-chmod 755 /usr/local/bin/vault-unlock.sh
-
-/usr/local/bin/vault-unlock.sh
-# Feed and remember the password for vault
-
 
 ################################################################################
