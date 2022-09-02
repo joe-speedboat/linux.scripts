856ae993060234e544aa009f6a8104029e3e65a4
Christian Ruettimann <chris@bitbull.ch>
2022-09-02 07:45:44 +0200

    tested with file and stdin with linebreaks

---
+++ telegram-send.sh
@@ -0,0 +1,49 @@
+#!/bin/bash
+
+# Copyright (c) Chris Ruettimann <chris@bitbull.ch>
+# This software is licensed to you under the GNU General Public License.
+# There is NO WARRANTY for this software, express or
+# implied, including the implied warranties of MERCHANTABILITY or FITNESS
+# FOR A PARTICULAR PURPOSE. You should have received a copy of GPLv2
+# along with this software; if not, see
+# http://www.gnu.org/licenses/gpl.txt
+
+### DESCRIPTION ############################################################
+# Send Telegram messages via cli, from stdin or by ARG1
+# - Setup bot and get auth token
+#   - https://www.toptal.com/python/telegram-bot-tutorial-python
+# - Setup Chat Group
+#   - add @getidsbot to your group
+#   - get chat-id and kick @getidsbot from group
+# - Configure this script vars
+############################################################################
+
+AUTH='9999999999:AAAAAAAbbbbbcccccccdddd-ddddddddddd'
+CHAT='-9999999999999'
+
+
+if [ "$1" == '-f' ]
+then
+   curl -s -F "chat_id=$CHAT" -F document=@$2 https://api.telegram.org/bot$AUTH/sendDocument
+   echo
+   echo INFO: Sent file: $2
+   exit 0
+fi
+
+if [ $# -eq 0 ]
+then 
+   MSG=$(</dev/stdin)
+elif [ $# -gt 0 ]
+then
+   MSG="$*"
+else
+  echo ERROR, please handover message by stdin or ARG
+  exit 1
+fi
+
+MSG_ENC="$(echo """$MSG""" |  curl -Gso /dev/null -w %{url_effective} --data-urlencode @- '' | cut -c 3- )"
+
+curl -s -k -X POST "https://api.telegram.org/bot$AUTH/sendMessage?chat_id=$CHAT&text=$MSG_ENC"
+echo
+echo INFO: Sent message: $MSG
+
