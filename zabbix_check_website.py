d3fb554bb1e3f0466a79dd5ca88909971b90ab8c
Christian Ruettimann <chris@bitbull.ch>
2023-01-24 15:13:39 +0100

    disable ssh warnings

--- zabbix_check_website.py
+++ zabbix_check_website.py
@@ -12,10 +12,13 @@
 # http://www.gnu.org/licenses/gpl.txt
 
 import requests
+from requests.exceptions import SSLError
 import re
 import sys
 import warnings
-from requests.packages.urllib3.exceptions import InsecureRequestWarning
+import urllib3
+urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
+
 
 # print debug information
 print_debug = False
@@ -29,39 +32,26 @@ fqdn = sys.argv[1]
 
 url = 'https://' + fqdn
 site_blocked_words = ['dnswatch.watchguard.com', ' denied by ']
+
 # as default, we assume site is not blocked, and we invert if any of the words get found
 site_works = True
 
-# to get around ssl-inspection we do not care vor certificate errors
-with warnings.catch_warnings():
-    warnings.filterwarnings("ignore", category=InsecureRequestWarning)
-    try:
-        response = requests.get(url, verify=False)
-        response.raise_for_status()
-        html = response.text
+# to get around ssl-inspection we do not care for certificate errors
+try:
+  response = requests.get(url, verify=False)
+  html = response.text
 
-        for word in site_blocked_words:
-            if re.search(word, html):
-              site_works = False
-              if print_debug:
-                print(f'{word} found on {url}')
-            else:
-              if print_debug:
-                print(f'{word} not found on {url}')
+  for word in site_blocked_words:
+    if re.search(word, html):
+      site_works = False
+      if print_debug:
+        print(f'{word} found on {url}')
+    else:
+      if print_debug:
+        print(f'{word} not found on {url}')
 
+except SSLError as e:
+  print("Suppressed SSL Error:", e)
 
-    except requests.exceptions.HTTPError as errh:
-      if print_debug:
-        print ("HTTP Error:",errh)
-    except requests.exceptions.ConnectionError as errc:
-      if print_debug:
-        print ("Error Connecting:",errc)
-    except requests.exceptions.Timeout as errt:
-      if print_debug:
-        print ("Timeout Error:",errt)
-    except requests.exceptions.RequestException as err:
-      if print_debug:
-        print ("Something went wrong",err)
-
-print("SITE_TEST:" + fqdn + "=" + str(site_works))
+print("SITE:" + fqdn + "=" + str(site_works))
 
