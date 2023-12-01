0b069365b9bf6c0bb7d7ee7fe1cefcc613647c3b
Christian Ruettimann <chris@control.sun.bitbull.ch>
2023-12-01 11:09:23 +0100

    devel

--- shell/package-health-check.sh
+++ shell/package-health-check.sh
@@ -154,8 +154,9 @@ check_public_repos() {
     fi
 
     for url in $repo_urls; do
-        ip=$(getent hosts $(echo $url | sed -e 's|^[^/]*//||' -e 's|/.*$||') | cut -d' ' -f1)
-        log debug "Checking repo URL: $url, resolved IP: $ip"
+        fqdn=$(echo $url | sed -e 's|^[^/]*//||' -e 's|/.*$||')
+        ip=$(getent hosts $fqdn 2>&1 | cut -d' ' -f1 | head -n1)
+        log debug "Checking repo FQDN: $fqdn, resolved IP: $ip"
         if [[ $ip =~ ^[0-9]+\.[0-9]+\.[0-9]+\.[0-9]+$ ]]; then
             if ! [[ $ip =~ ^(10|172\.(1[6-9]|2[0-9]|3[0-1])|192\.168)\. ]]; then
                 log $public_repos_report "Public IP detected in repo: $url ($ip)"
