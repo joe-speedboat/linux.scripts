49a91433fa044fa71548a68d72db78a78cfd54a7
Chris Ruettimann <chris@bitbull.ch>
2024-06-12 12:53:12 +0200

    rewrite and shrink down, gpt is not yet clever enough :-)

--- shell/report_local_user_logins.sh
+++ shell/report_local_user_logins.sh
@@ -9,9 +9,6 @@
 # along with this software; if not, see
 # http://www.gnu.org/licenses/gpl.txt
 
-# print debug statements
-DEBUG=0
-
 # Define a variable for excluded logins
 EXCLUDE_LOGINS='hostname1:user1:1.2.3.4
 hostname2:user2:1.2.3.4'
@@ -29,16 +26,13 @@ extract_login_users() {
 check_user_logins() {
     local user=$1
     local logins=()
-    [ $DEBUG -ne 0 ] && echo "------ Checking user logins for user: $user"
     logins_found=0
     # Determine if the system uses journalctl (systemd) or /var/log/auth.log (syslog)
     if command -v journalctl &> /dev/null; then
         # RHEL-ish systems with journalctl
-        [ $DEBUG -ne 0 ] && echo "------ Using journalctl to check logins for user: $user"
         logins=$(journalctl --since "25 hours ago" | grep -E "sshd.*Accepted .* for $user ")
     elif [ -f /var/log/auth.log ]; then
         # Ubuntu systems with /var/log/auth.log
-        [ $DEBUG -ne 0 ] && echo "------ Using /var/log/auth.log to check logins for user: $user"
         logins=$(grep -E "sshd.*Accepted .* for $user " /var/log/auth.log --since="25 hours ago")
     else
         echo "Unsupported logging system."
@@ -49,23 +43,14 @@ check_user_logins() {
     if [ -n "$logins" ]; then
         echo "$logins" | while read -r login; do
             local src_ip=$(echo "$login" | grep -oE '([0-9]{1,3}\.){3}[0-9]{1,3}')
-            [ $DEBUG -ne 0 ] && echo "Checking if login for user: $user from IP: $src_ip is excluded"
             if ! is_excluded_login "$user" "$src_ip"; then
-                [ $DEBUG -ne 0 ] && echo "Login for user: $user from IP: $src_ip is not excluded / logins_found=$logins_found"
                 echo "Login detected for user: $user from IP: $src_ip"
-                logins_found=1
-                [ $DEBUG -ne 0 ] && echo "logins_found=$logins_found"
+                return 1
             else
                 echo "Excluded login detected for user: $user from IP: $src_ip"
+                return 0
             fi
         done
-    fi
-    if [[ $logins_found -gt 0 ]]; then
-        logins_found=1
-        [ $DEBUG -ne 0 ] && echo "------ Value of logins_found after setting: $logins_found"
-    else
-        [ $DEBUG -ne 0 ] && echo "------ No login found for user: $user"
-        true
     fi
 }
 
@@ -74,18 +59,14 @@ is_excluded_login() {
     local user=$1
     local src_ip=$2
     local hostname=$(hostname)
-    [ $DEBUG -ne 0 ] && echo "Checking exclusions for user: $user, hostname: $hostname, source IP: $src_ip"
     while IFS=: read -r ex_hostname ex_user ex_ip; do 
-        [ $DEBUG -ne 0 ] && echo "Comparing with ex_hostname: $ex_hostname, ex_user: $ex_user, ex_ip: $ex_ip"
         if [[ "$hostname" =~ $ex_hostname ]] && \
            [[ "$user" =~ $ex_user ]] && \
            [[ "$src_ip" =~ $ex_ip ]]; then
-           [ $DEBUG -ne 0 ] && echo "Match found. Excluding login for user: $user from IP: $src_ip"
             return 0 # Excluded login
         fi
     done <<< "$EXCLUDE_LOGINS"
 
-    [ $DEBUG -ne 0 ] && echo "No match found. Not excluding login for user: $user from IP: $src_ip"
     return 1 # Not an excluded login
 }
 
@@ -95,20 +76,18 @@ declare -g logins_found_at_all=0
 login_users=$(extract_login_users)
 
 for user in $login_users; do
-    [ $DEBUG -ne 0 ] && echo "--- function check_user_logins $user / logins_found_at_all=$logins_found_at_all"
-    if check_user_logins "$user"; then
+    if ! check_user_logins "$user"; then
         logins_found_at_all=1
     fi
-    [ $DEBUG -ne 0 ] && echo "--- Value of logins_found_at_all after checking user $user: $logins_found_at_all"
+    echo -n
 done
 
-[ $DEBUG -ne 0 ] && echo "Final value of logins_found_at_all: $logins_found_at_all"
 
 if [ $logins_found_at_all -gt 0 ]; then
     echo "Logins found for one or more users in the last 25 hours."
     exit 1
 else
-    echo "No logins found for the checked users in the last 25 hours. All good."
+    echo "No unwanted logins found for the checked users in the last 25 hours. All good."
     exit 0
 fi
 
